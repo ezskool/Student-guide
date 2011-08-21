13572368367108ac98ad20db7937bbf4517d3053
haakondr <haakondr@gmail.com>
2011-08-21 17:22:11 +0200

    added course metadata in assets folder, as a backup if the phone is not connected to internet.
    Restructured main menu and packages

--- src/studentguiden/ntnu/misc/Globals.java
+++
@@ -1,8 +0,0 @@
-package studentguiden.ntnu.misc;
-
-
-public class Globals {
-
-	public static final String courseListURL = "http://www.ime.ntnu.no/api/course/-";
-	public static boolean hasCalledCourseDownloader = false;
-}
